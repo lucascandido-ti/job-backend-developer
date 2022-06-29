e09a2b55591107fec586c14d69b56d716d9b2fa0
lucas-candido <sistema2@innovationbrindes.com.br>
2022-06-29 09:08:19 -0300

    Adding model and migration to product table

---
+++ database/migrations/2022_06_29_091134_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable(false)->unique();
+            $table->decimal('price', 8, 2, true)->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->string('category')->nullable(false);
+            $table->string('image_url')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
